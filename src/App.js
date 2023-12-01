02e73b363bd4c19704a8c9c3c588cf00465cd223
anaysah <ajaysahu0606@gmail.com>
2023-12-01 11:39:52 +0530

    added loading and error state

--- src/App.js
+++ src/App.js
@@ -3,24 +3,38 @@ import MovieCard from "./MovieCard";
 
 const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=23ec9fb9";
 
-// const movie1 = {
-//     "Title": "Star Wars: Episode IV - A New Hope",
-//     "Year": "1977",
-//     "imdbID": "tt0076759",
-//     "Type": "movie",
-//     "Poster": "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg"
-// }
-
 const App = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const handleSearchInputChange = (e) => {
+        const newSearchTerm = e.target.value;
+        setSearchTerm(newSearchTerm);
+        searchMovies(newSearchTerm);
+    };
     
 
     const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
-        console.log(data.Search);
-        setMovies(data.Search);
+        try {
+            setLoading(true);
+            const response = await fetch(`${API_URL}&s=${title}`);
+            const data = await response.json();
+
+            if (data.Error) {
+                setError(data.Error);
+                setMovies([]);
+            } else {
+                setError(null);
+                setMovies(data.Search || []);
+            }
+        } catch (error) {
+            setError('An error occurred while fetching data.');
+            setMovies([]);
+        } finally {
+            setLoading(false);
+        }
     }
 
     useEffect(() => {
@@ -30,31 +44,18 @@ const App = () => {
     return (
         <div>
             <h1>Movies G</h1>
-            <input placeholder="Search a Movie" value={searchTerm} onChange={(e) => {setSearchTerm(e.target.value)}} />
-            <button onClick={() => {searchMovies(searchTerm) }}>Search</button>
+            <input placeholder="Search a Movie" value={searchTerm} onChange={handleSearchInputChange} />
+            <button onClick={() => { searchMovies(searchTerm) }}>Search</button>
 
-            {/* <MovieCard Title={movie1.Title}  Year = {movie1.Year} Type={movie1.Type} Poster={movie1.Poster}/> */}
-            {/* <MovieCard movie={movies[2]}/> */}
-
-            {
-                movies?.length > 0 ? (
-                    <div>
-                        {
-                            // movies.map((movie) => {
-                            //     return <MovieCard movie={movie} key={movie.imdbID} />
-                            // })
-                            movies.map((movie) => (
-                                <MovieCard movie={movie} key={movie.imdbID} />
-                            ))
-
-                        }
-                    </ div >
-                ) : (
-                    <div>
-                        <h2>No Movies Found</h2>
-                    </div>
-                )
-            }
+            {loading ? (
+                <p>Loading...</p>
+            ) : error ? (
+                <p>Error: {error}</p>
+            ) : (
+                movies.map((movie) => (
+                    <MovieCard movie={movie} key={movie.imdbID} />
+                ))
+            )}
         </div>
     )
 }
